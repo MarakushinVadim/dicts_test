32842163db0347abf64cae9bbccbfc8d371bbf0f
Marakushin Vadim <marakushin.vadim@gmail.com>
2023-12-31 00:45:45 +0300

    3

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,9 +1,15 @@
+import pytest
+
 from utils import dicts
 
+@pytest.fixture
+def data_fixture():
+    return {"vcs": "mercurial"}
 
-def test_get_val():
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs", 'git') == "mercurial"
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+
+def test_get_val(data_fixture):
+    assert dicts.get_val(data_fixture, "vcs", 'git') == "mercurial"
+    assert dicts.get_val(data_fixture, "vcs") == "mercurial"
     assert dicts.get_val({}, "vcs", 'git') == "git"
     assert dicts.get_val({}, "vcs", 'bazaar') == "bazaar"
-    assert dicts.get_val({"vcs": "mercurial"}, "git", 'bazaar') == "bazaar"
+    assert dicts.get_val(data_fixture, "git", 'bazaar') == "bazaar"
